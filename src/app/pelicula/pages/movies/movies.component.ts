89bdd77994a2ba3e665e3c32189e933dd0796b0d
Gabriel Sardina <gabinex15@gmail.com>
2022-05-19 00:41:36 -0300

    add service metod movieByYear, ts and html modific

--- src/app/pelicula/pages/movies/movies.component.ts
+++ src/app/pelicula/pages/movies/movies.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent implements OnInit {
-
-  constructor() { }
+  movies: any[] = [];
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    console.log('funcion1: desde ngOnInit');
+    this.moviesService.moviesByYear().subscribe((resp) => {
+      console.log(resp);
+      this.movies = resp.results;
+      console.log(this.movies);
+    });
   }
 
+  // buscarPorAnio() {
+  //   console.log('funcion1');
+  //   this.moviesService.moviesByYear().subscribe((resp) => console.log(resp));
+  // }
 }
--- src/app/pelicula/services/movies.service.ts
+++ src/app/pelicula/services/movies.service.ts
@@ -1,9 +1,42 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
+  private url: string =
+    'https://moviesminidatabase.p.rapidapi.com/movie/byYear/2021/';
+
+  private get getHttpHeaders() {
+    return {
+      headers: new HttpHeaders({
+        'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com',
+        'X-RapidAPI-Key': '2272da370bmsh959fdd726380f99p1b51d0jsnd82d9badb07c',
+      }),
+    };
+  }
+
   constructor(private http: HttpClient) {}
+
+  public moviesByYear(): Observable<any> {
+    const header = new HttpHeaders({
+      'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com',
+      'X-RapidAPI-Key': '2272da370bmsh959fdd726380f99p1b51d0jsnd82d9badb07c',
+    });
+    const params = new HttpParams().set('page_size', '10').set('page', '1');
+    return this.http.get<any>(this.url, { headers: header, params: params });
+  }
+
+  // (this.url, {
+  //   headers:header,
+  //   params: params,
+  // })
+  // public traducirTexto(): Observable<any> {
+  //   const body = new HttpParams()
+  //     .set('q', 'text')
+  //     .set('target', 'target')
+  //   return this.http.get<any>(this.url, this.getHttpHeaders, body);
+  // }
 }
